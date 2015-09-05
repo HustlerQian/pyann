8ad25b31c49e1f5b6094308b7a7cc78ddb6d4829
racaljk <cthulhujk@gmail.com>
2015-09-05 15:42:16 +0800

    #complete backpropagate algorithm(subprocessing)  the last thing is to calculation combined error  and set stop iteration condition #simplified variable name

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# ![logo](http://i1.tietuku.com/876fae1c923a713f.png)
+﻿# ![logo](http://i1.tietuku.com/876fae1c923a713f.png)
 ![license](https://img.shields.io/github/license/mashape/apistatus.svg)![pythontype](https://img.shields.io/badge/python-2.7-blue.svg)![status](https://img.shields.io/badge/status-updating-orange.svg)<br>
 pyann是以KISS(Keep It Simple Stupid)哲学为原则构建的逻辑清晰易于学习的人工神经网络包，我会不断的完善它。
 
--- network/bpann.py
+++ network/bpann.py
@@ -33,68 +33,83 @@ class bpann(object):
         self.iteration = 0
         self.convergence =0.0
 
-    def init(self,inputNodeNum = 4,
-        hiddenNodeNum = 5,
-        outputNodeNum = 3,
+    def init(self,inputLayerNodeNum = 4,
+        hiddenLayerNodeNum = 5,
+        outputLayerNodeNum = 3,
         maxIteration = 1000,
-        initWeights_Start = -1,
-        initWeights_End = 1,
+        biasRange = [0,1],
+        weightRange = [-1,1]
         learningRate = 0.85
         ):
-        self.inputNodeNum = inputNodeNum
-        self.hiddenNodeNum = hiddenNodeNum
-        self.outputNodeNum = outputNodeNum
-        self.maxIteration = maxIteration
-        self.learningRate = learningRate
+        #constant
+        self.inode = inputLayerNodeNum
+        self.hnode = hiddenLayerNodeNum
+        self.onode = outputLayerNodeNum
+        self.maxIter = maxIteration
+        self.lr = learningRate
+        
+        #initial bias
+        self.hb = random.uniform(biasRange[0],biasRange[1])
+        self.ob = random.uniform(biasRange[0],biasRange[1])
 
-        #inital inputLayer-hiddenLayer and hiddenLayer-outputLayer weights
-        self.ihWeights = [[random.uniform(initWeights_Start,initWeights_End) for t in xrange(hiddenNodeNum)]for x in xrange(inputNodeNum)]
-        self.hoWeights = [[random.uniform(initWeights_Start,initWeights_End) for t in xrange(outputNodeNum)]for x in xrange(hiddenNodeNum)]
+        #initial inputLayer-hiddenLayer and hiddenLayer-outputLayer weights
+        self._init_hWeights()
+        self._init_oWeights()
 
-        #to save input and output data of hidden layer and output
-        #layer for back propagation
-        self.hdata = [0] * self.hiddenNodeNum
-        self.odata = [0] * self.outputNodeNum
+        #save input and output data 
+        self.hdata = [0] * self.hnode
+        self.odata = [0] * self.onode
 
     def train(self,trainData):
         learned = False
         while not learned:
-            for x in trainData:
-                self.feedForward(x)
-                self.backPropagate(x)
-            if self.convergence == 0.0 or \
-                self.iteration >= self.maxIteration:
-                print 'learned'
-                learned = True
-            else:
-                print 'global error:%d' % self.convergence
+            for record in trainData:
+                self.feedForward(record)
+                self.backPropagate(record)
+            
+    def feedForward(self,data):
+        for x in xrange(self.hnode):
+            s = 0
+            for t in xrange(len(data)-1):
+                s += data[t] * self.hw[t][x] + self.hb
+            self.hdata[x] = [s,self._sigmoid(s)]
 
-    def feedForward(self,trainDataPart):
-        for hnode in xrange(self.hiddenNodeNum):
-            net = 0
-            for x in xrange(len(trainDataPart)-1):
-                net += trainDataPart[x] * self.ihWeights[x][hnode]#default bias=0
-        self.hdata[hnode] = [net,self.activationFunc(net)]
+        for x in xrange(self.onode):
+            s = 0
+            for t in xrange(self.hnode):
+                s += self.hdata[t][1] * self.ow[t][x] +self.ob
+            self.odata[x] = [s,_sigmoid(s)]
 
-        for onode in xrange(self.outputNodeNum):
-            net = 0
-            for x in xrange(len(self.hdata)):
-                net += self.hdata[x][1] * self.hoWeights[x][onode]
-        self.odata[onode] = [net,self.activationFunc(net)]
+    def backPropagate(self,data):
+        Eo = [0] * self.onode
+        Eh = [0] * self.hnode
+        #update hiddenLayer-outputLayer weights
+        for x in xrange(self.onode):
+            Eo[x] = self.odata[x][1] * (1-self.odata[x][1]) * (data[-1] - self.odata[x][-1])
+            for t in xrange(self.hnode):
+                self.ow[t][x] += self.lr * self.hdata[t][1] * Eo[x]
+                self.ob += self.lr * Eo[x]  
+        #update inputLayer-hiddenLayer weights
+        for x in xrange(self.hnode):
+            s =0
+            for p in xrange(self.onode):
+                s += self.ow[x][p] * Eo[p]
+            Eh[x] = self.hdata[x][1] * (1-self.hdata[x][1]) * s
+            for t in xrange(self.inode):
+                self.hw[t][x] += self.lr * data[t] * self.Eh[x] 
+                self.hb +=self.lr * Eh[x]
 
-    def backPropagate(self,trainDataPart):
-        dsigmoid = lambda x:1 - x**2
+        #todo:cacl global error
 
-        #todo:update hiddenLayer-outputLayer weights
-        #todo:update inputLayer-hiddenLayer weights
-        #cacl global error
-        for x in xrange(self.outputNodeNum):
-            self.convergence = self.learningRate * (trainDataPart[-1] - self.odata[x][1])**2
+    def expectedOutput(self,data):
+        pass
 
-    def expectedOutput(self,data):  
-        if len(data) != self.inputNodeNum:
-            print 'incorrect number of input data'
-            #todo
-
-    def activationFunc(self,x):          
+    def _sigmoid(self,x):
         return 1.0/(1.0 + math.exp(-x))
+    def _init_hWeights():
+        self.hw = [[random.uniform(weightRange[0],weightRange[1]) \
+        for t in xrange(self.hnode)]for x in xrange(self.inode)]
+    def _init_oWeights():
+        self.ow = [[random.uniform(weightRange[0],weightRange[1]) \
+        for t in xrange(self.onode)]for x in xrange(self.hnode)] 
+
